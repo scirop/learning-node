91df4340e7a020d03b006158c15a7abe7592d3c9
Swarup Sahoo <swarupsahoo@Swarups-MacBook.local>
2018-11-02 19:54:34 -0500

    updated

--- index.js
+++ index.js
@@ -18,16 +18,22 @@ var server = http.createServer(function(req, res){
 
   // Get the path from that URL
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Get query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // Get the http method
+  var method = req.method.toLowerCase();
 
   // Send the response
   res.end('Hello World\n');
 
   // Log the user path
-  console.log('Request is received on:'+trimmedPath);
+  console.log('Request is received on:'+trimmedPath+' with method: ' + method +' and with these query string parameters', queryStringObject);
 })
 
 // Start the server and have it listen to port 3000
 server.listen(3000, function(){
-  console.log('Listening on port 3000');
+  console.log('Listening on port http://localhost:3000');
 })
